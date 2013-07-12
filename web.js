bab636d04a77a56b0c8a1c14ac9941021e9efab5
alexsimps <alexsimps84@gmail.com>
2013-07-12 03:29:16 +0000

    another edit of web.js

--- web.js
+++ web.js
@@ -1,10 +1,10 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-var fills Buffer(256);
+//var fills Buffer(256);
 app.get('/', function(request, response) {
     response.send('wtf');
-    fills=fs.readFileSync;
+//    fills=fs.readFileSync;
 //    response.send(buf.toString(fills);
  //   response.send(buf.toString(fs.writeFile(index.html)));
 
