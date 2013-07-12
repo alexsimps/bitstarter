afbd50278216a95088a4061fb253054231bec653
alexsimps <alexsimps84@gmail.com>
2013-07-12 05:32:54 +0000

    another edit of web.js

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ var app = express.createServer(express.logger());
 var fills;
 app.get('/', function(request, response) {
 
-    fills = fs.readFileSync('index1.html');
+    fills = fs.readFileSync('index.html');
 
     response.send(fills.toString());
 
